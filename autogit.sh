3f1202d7d7aedf62b9db2ca077dffdcdaead128f
Carlos Sanchez <crs09e@my.fsu.edu>
2016-10-26 21:57:50 -0400

    Added autogit file for easier git

---
+++ autogit.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+# Carlos Sanchez - 2016
+# randomouscrap98@aol.com
+
+# This script performs a git add --all, a git commit with
+# the first argument to the script as the message, and a
+# git push all in one. I don't usually care about the 
+# individual steps, so this makes it easier.
+
+if [ $# -ne 1 ]
+then
+   echo "Needs one argument. Example:"
+   echo "./`basename $0` \"My commit message\""
+   exit 1
+fi
+
+echo "Autogit starting..."
+git add --all
+git commit -m "$1"
+git push
+echo "Autogit complete!"
+
+exit 0
